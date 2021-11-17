b3ab15ff5f66acd33bf4a1f00adaf6d40faa778b
iyume <iyumelive@gmail.com>
2021-11-17 11:01:32 +0800

    feat: add overload source picker.

--- doc/pdoc/pycode/overload.py
+++ doc/pdoc/pycode/overload.py
@@ -1,12 +1,15 @@
 import ast
 import inspect
-from typing import List, Dict, Union, Optional, Any
+from typing import List, Dict, Union, Optional, get_args
 from copy import deepcopy
 from inspect import Signature
 
 
+T_Function = Union[ast.FunctionDef, ast.AsyncFunctionDef]
+
+
 def extract_all_overloads(source: str) -> 'OverloadPicker':
-    picker = OverloadPicker(source)
+    picker = OverloadPicker()
     astmodule = ast.parse(source)
     picker.visit(astmodule)
     return picker
@@ -14,44 +17,23 @@ def extract_all_overloads(source: str) -> 'OverloadPicker':
 
 class OverloadPicker(ast.NodeVisitor):
     """
-    Python source code parser to pick up overload function signatures.
+    Python ast visitor to pick up overload function source.
     """
 
-    def __init__(self, source: str) -> None:
-        self.source: str = source
-        self.globals: Dict[str, Any] = {}
+    def __init__(self) -> None:
         self.context: List[str] = []
         self.current_class: Optional[ast.ClassDef] = None
-        self.overloads: Dict[str, List[Signature]] = {}
-        self.implements: Dict[str, Any] = {}
-        self.typing: Optional[str] = None
-        self.typing_overload: Optional[str] = None
+        self.overloads: Dict[str, List[str]] = {}
+        self.functions: Dict[str, T_Function] = {}
+        self.typing: List[str] = []
+        self.typing_overload: List[str] = []
         super().__init__()
-        # exec to get the module __globals__
-        # aim to get function signature
-        try:
-            exec(self.source, self.globals)
-        except Exception:
-            return
-
-    def get_qualname_for(self, name: str) -> Optional[List[str]]:
-        if self.current_function:
-            if self.current_class:
-                return self.context[:-1] + [name]
-            else:
-                return None
-        else:
-            return self.context + [name]
 
     def is_overload(
         self,
-        node: Union[ast.FunctionDef, ast.AsyncFunctionDef]
+        node: T_Function
     ) -> bool:
-        overload_ids = []
-        if self.typing:
-            overload_ids.append('%s.overload' % self.typing)
-        if self.typing_overload:
-            overload_ids.append(self.typing_overload)
+        overload_ids = [f'{i}.overload' for i in self.typing] + self.typing_overload
         for decorator in node.decorator_list:
             if ast.unparse(decorator) in overload_ids:
                 return True
@@ -59,26 +41,31 @@ class OverloadPicker(ast.NodeVisitor):
 
     def add_overload_signature(
         self,
-        node: Union[ast.FunctionDef, ast.AsyncFunctionDef]
+        node: T_Function
     ) -> None:
-        qualname = self.get_qualname_for(node.name)
-        if qualname:
-            overloads = self.overloads.setdefault(".".join(qualname), [])
+        qualname = node.name
+        if self.current_class:
+            qualname = f'{self.current_class.name}.{node.name}'
+        overloads = self.overloads.setdefault(qualname, [])
         node = deepcopy(node)
+        node.decorator_list.clear()
+        unwrap_source = ast.unparse(node)
+        overloads.append(unwrap_source)
 
     def visit_Import(self, node: ast.Import) -> None:
         for name in node.names:
             if name.name == 'typing':
-                self.typing = name.asname or name.name
+                self.typing.append(name.asname or name.name)
             elif name.name == 'typing.overload':
-                self.typing_overload = name.asname or name.name
+                self.typing_overload.append(name.asname or name.name)
 
     def visit_ImportFrom(self, node: ast.ImportFrom) -> None:
         for name in node.names:
             if node.module == 'typing' and name.name == 'overload':
-                self.typing_overload = name.asname or name.name
+                self.typing_overload.append(name.asname or name.name)
 
     def visit_Try(self, node: ast.Try) -> None:
+        # ignore try stmt special in module
         return
 
     def visit_ClassDef(self, node: ast.ClassDef) -> None:
@@ -88,22 +75,18 @@ class OverloadPicker(ast.NodeVisitor):
             self.current_class = node
             self.context.append(node.name)
             for child in node.body:
-                self.visit(child)
+                if isinstance(child, get_args(T_Function)):
+                    self.visit(child)
             self.context.pop()
             self.current_class = None
 
     def visit_FunctionDef(self, node: ast.FunctionDef) -> None:
         """Handles FunctionDef node and set context."""
-        # ignore function inner function
-        if self.current_function is None:
-            if self.is_overload(node):
-                self.add_overload_signature(node)
-            self.context.append(node.name)
-            self.current_function = node
-            for child in node.body:
-                self.visit(child)
-            self.context.pop()
-            self.current_function = None
+        self.context.append(node.name)
+        self.functions[node.name] = node
+        if self.is_overload(node):
+            self.add_overload_signature(node)
+        self.context.pop()
 
     def visit_AsyncFunctionDef(self, node: ast.AsyncFunctionDef) -> None:
         """Handles AsyncFunctionDef node and set context."""
